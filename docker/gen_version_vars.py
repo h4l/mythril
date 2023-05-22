4d86ca7f295e69db257460d09863ecb4c74ed827
Hal Blackburn <hwtb2@cam.ac.uk>
2023-05-22 05:25:09 +0100

    refactor: move image files under ./docker

--- docker/gen_version_vars.py
+++ docker/gen_version_vars.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python3
 """Generate the contents of docker-bake.versions.json from poetry.lock"""
 import json
-import toml
+import tomllib
 
 
 def main():
-    with open("poetry.lock") as f:
-        lockfile = toml.loads(f.read())
+    with open("poetry.lock", "rb") as f:
+        lockfile = tomllib.load(f)
 
     packages = {p["name"]: p for p in lockfile["package"]}
 
     version_vars = json.dumps(
         {
             "MYTHRIL_VERSION": packages["mythril"]["version"],
-            "Z3SOLVER_VERSION": packages["z3-solver"]["version"],
             "BLAKE2B_VERSION": packages["blake2b-py"]["version"],
         },
-        indent=2
+        indent=2,
     )
     print(version_vars)
+
 
 if __name__ == "__main__":
     main()
